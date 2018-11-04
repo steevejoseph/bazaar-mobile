b0b56a72a26aa7bf03c1ba5035221dec97ffa6f6
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-11-04 12:08:49 -0500

    test pre-commit linter, again

--- bazaar_mobile/src/components/Button.js
+++ bazaar_mobile/src/components/Button.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
 
 // AKA TouchableHighlight and/or TouchableOpacity (via RN docs)
 // highlight: button turned a diff color when pressed/
@@ -17,27 +17,27 @@ const Button = props => {
 
 const styles = {
   textStyle: {
-    alignSelf: "center",
-    color: "#007aff",
+    alignSelf: 'center',
+    color: '#007aff',
     fontSize: 16,
-    fontWeight: "600",
+    fontWeight: '600',
     paddingTop: 10,
-    paddingBottom: 10
+    paddingBottom: 10,
   },
 
   buttonStyle: {
     // flex 1 means "expand to fill entire space"
     flex: 0,
     // align this element with flexbox rules
-    alignSelf: "stretch",
-    backgroundColor: "#fff",
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: "#007aff",
+    borderColor: '#007aff',
     marginLeft: 5,
     marginRight: 5,
     height: 50,
-    width: 350
-  }
+    width: 350,
+  },
 };
 export default Button;
