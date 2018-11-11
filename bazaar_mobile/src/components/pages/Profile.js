ee7072594a4716da2293349b10b95beb42975d8f
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-11-11 14:56:09 -0500

    add logout navigation

--- bazaar_mobile/src/components/pages/Profile.js
+++ bazaar_mobile/src/components/pages/Profile.js
@@ -5,16 +5,49 @@ import { Card, CardSection, Button, Spinner, Header, Footer } from '../common';
 
 class Profile extends Component {
   render() {
+    const { infoTextStyle, infoContainerStyle } = styles;
     return (
       <Card>
         <ScrollView>
-          <Text>Placeholder</Text>
+          <CardSection style={infoContainerStyle}>
+            <Text style={infoTextStyle}>First Name:</Text>
+          </CardSection>
+          <CardSection style={infoContainerStyle}>
+            <Text style={infoTextStyle}>Last Name:</Text>
+          </CardSection>
+          <CardSection style={infoContainerStyle}>
+            <Text style={infoTextStyle}>Email:</Text>
+          </CardSection>
+          <CardSection style={infoContainerStyle}>
+            <Text style={infoTextStyle}>Phone number:</Text>
+          </CardSection>
         </ScrollView>
-        <Button style={{ backgroundColor: 'red', marginBottom: 10, alignSelf: 'stretch' }}>Logout</Button>
+        <Button
+          style={{ backgroundColor: 'red', marginBottom: 10, alignSelf: 'stretch' }}
+          onPress={() => Actions.Landing({ type: 'reset' })}
+        >
+          Logout
+        </Button>
         <Footer footerText="" />
       </Card>
     );
   }
 }
+
+const styles = {
+  infoContainerStyle: {
+    justifyContent: 'flex-start',
+  },
+  infoTextStyle: {
+    color: '#000',
+    fontSize: 16,
+    textAlign: 'left',
+    fontWeight: '600',
+    alignSelf: 'stretch',
+    paddingTop: 10,
+    paddingBottom: 10,
+    opacity: 0.5,
+  },
+};
 
 export { Profile };
