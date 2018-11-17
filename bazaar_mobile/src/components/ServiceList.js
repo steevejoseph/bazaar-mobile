da2a8f23b15d198389a44a21c82a3421e99bcb22
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-11-17 12:48:29 -0500

    Simplify MyServices & Explore

--- bazaar_mobile/src/components/ServiceList.js
+++ bazaar_mobile/src/components/ServiceList.js
@@ -1,29 +1,24 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { FlatList } from 'react-native';
 import { fetchAllServices } from '../actions';
 import ServiceListItem from './ServiceListIem';
 
 class ServiceList extends Component {
-  componentWillMount() {
-    this.props.fetchAllServices();
-  }
-
   renderItem(service) {
     return <ServiceListItem service={service} />;
   }
 
   render() {
-    return <FlatList style={{ backGroundColor: '#f8f8f8' }} data={this.props.services} renderItem={this.renderItem} />;
+    return (
+      <FlatList
+        style={{ backGroundColor: '#f8f8f8' }}
+        data={this.props.services}
+        renderItem={this.renderItem}
+        keyExtractor={(item, index) => index.toString()} // TODO: make KeyExtractor pull service ID.
+      />
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  services: state.service.services,
-});
-
-export default connect(
-  mapStateToProps,
-  { fetchAllServices }
-)(ServiceList);
+export default ServiceList;
